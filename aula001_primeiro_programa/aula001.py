2307dd5220552e360537940034243cc0b6077786
Gilber-Mattos <gilbermendesjf@outlook.com>
2024-04-12 21:25:22 -0300

    hello world

---
+++ aula001_primeiro_programa/aula001.py
@@ -0,0 +1,17 @@
+# Autor: Gilber Mattos Mendes
+# Data: 12/04/2024
+# Primeiro Programa Python
+
+# Importando Bibliotecas
+import os
+
+
+# Limpando o Terminal
+os.system('cls')
+
+# O comando Print() executa uma saída via terminal
+
+print('-' * 70)
+print('Hello World!')
+print('-' * 70)
+print()

96bbbe9ee1ade130b7cc153e1e1f909d5a949af3
Gilber-Mattos <gilbermendesjf@outlook.com>
2024-04-17 21:41:43 -0300

    operadores_aritimeticos

---
+++ aula005_operadores_aritimeticos/operadores_aritimeticos.py
@@ -0,0 +1,52 @@
+# imports
+import os
+
+
+# limpar o terminal
+os.system('cls')
+
+print('-'*70)
+print('OPERADORES ARITIMÉTICOS')
+print('='*70)
+
+# Entrada de Dados
+print('--- SOMA')
+print('+'*70)
+parcela_1 = float(input('Entre com a 1ª parcela: '))
+parcela_2 = float(input('Entre com a 2ª parcela: '))
+
+print()
+print('--- SUBTRAÇÃO')
+print('-'*70)
+minuendo = float(input('Entre com o minuendo: '))
+subtraendo = float(input('Entre com a subtraendo: '))
+
+print()
+print('--- PRODUTO')
+print('-'*70)
+multiplicando = float(input('Entre com o multiplicando: '))
+multiplicador = float(input('Entre com o multiplicador: '))
+
+print()
+print('--- DIVISÃO')
+print('-'*70)
+dividendo = float(input('Entre como dividendo: '))
+divisor = float(input('Entre com o divisor: '))
+
+# Processamento
+soma = parcela_1 + parcela_2
+diferanca = minuendo - subtraendo
+produto = multiplicando * multiplicador
+quociente = dividendo / divisor
+
+# Saída
+print('='*70)
+print('RESULTADOS')
+print('-'*70)
+print(f'A soma de {parcela_1} + {parcela_2} é: {soma}')
+print(f'A subtração de {minuendo} - {subtraendo} é: {diferanca}')
+print(f'A multiplicação de {multiplicando} x {multiplicador} é: {produto}')
+print(f'A diferença de {dividendo} / {divisor} é {quociente}')
+
+# Seguindo os passos anteriores, desenvolva o restante
+# Acrescente a raiz quadrada e a rais cúbica.

d6863e4fa139f78a0ae30bcd21aac20291300e8f
Gilber-Mattos <gilbermendesjf@outlook.com>
2024-04-25 21:31:03 -0300

    Atividades Condicionais

--- atividades/atividade_001_condicionais/atividade_b.py
+++ atividades/atividade_001_condicionais/atividade_b.py
@@ -19,3 +19,36 @@ print('-'*70)
 numero1 = float(input('Digite o 1º número: '))
 numero2 = float(input('Digite o 2º número: '))
 numero3 = float(input('Digite o 3º numero: '))
+print('='*70)
+
+# QUAL NÚMERO É O MAIOR
+if (numero1 > numero2 and numero1 > numero3):
+    print(f'{numero1: .0f} é o Maior')
+    
+elif (numero2 > numero1 and numero2 > numero3):
+    print(f'{numero2: .0f} é o Maior')
+
+else:
+    print(f'{numero3: .0f} é o Maior')
+    
+print('-'*70)
+
+# QUAL NÚMERO É O MENOR
+if (numero1 < numero2 and numero1 < numero3):
+    print(f'{numero1: .0f} é o Menor')
+    
+elif (numero2 < numero1 and numero2 < numero3):
+    print(f'{numero2: .0f} é o Menor')
+
+else:
+    print(f'{numero3: .0f} é o Menor')
+    
+print('-'*70)
+
+# OS NÚMEROS SÃO IGUAIS?
+if (numero1 == numero2 == numero3):
+    print('Os números são iguais! ')
+else:
+    print('Os números não são iguais! ')
+
+print('-'*70)

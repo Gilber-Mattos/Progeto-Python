ddf2d797780c144fae70d512c473807a6c031c16
Gilber-Mattos <gilbermendesjf@outlook.com>
2024-05-09 21:20:03 -0300

    biblioteca random

---
+++ aula_007_bibliotecas/biblioteca_random.py
@@ -0,0 +1,31 @@
+import random
+import os
+
+
+os.system('cls')
+
+print('-'*70)
+print('BIBLIOTECA RANDOM')
+print('='*70)
+
+print('Número aleatório')
+numero_aleatorio = random.random()
+print(f'O número gerado foi: {numero_aleatorio}')
+print('.'*70)
+
+print('Número aleatório inteiro')
+aleatorio_inteiro = random.randint(1, 20)
+print(f'O número inteiro gerado foi: {aleatorio_inteiro}')
+print('.'*70)
+
+print('Número aleatório decimal no intervalo')
+aleatorio_decimal = random.uniform(1, 10)
+print(f'O número decimal gerado foi: {aleatorio_decimal}')
+print('.'*70)
+
+print('Sorteio em um lista')
+lista = ['Agata', 'Coly', 'Isis', 'Bia']
+nome_sorteado = random.choice(lista)
+print(f'Lista: {lista}')
+print(f'O nome escolhido foi: {nome_sorteado}')
+print('.'*70)
--- meu_projeto_01/projeto_inicial_completo.py
+++ meu_projeto_01/projeto_inicial_completo.py
@@ -3,16 +3,39 @@
 # Data Inicial: 25/04/2024
 
 import os
+import random
+import math
 
 
 os.system('cls')
 
 print('-'*70)
-print('IMPAR OU PAR? VOCÊ SABE?')
+print('GERE UM NÚMERO ALEATÓRIO E DESCUBRA SE É PAR OU IMPAR')
 print('-'*70)
 
-# Entrada
-numero = float(input('Digite um número: '))
-resposta = ''
+print('Número inteiro aleatório: ')
+numero_aleatorio = random.randint(1, 100)
+print(f'Número gerado: {numero_aleatorio}')
 
-# Processamento
+# ENTRADA
+decisao = input('Deseja gerar outro número? (SIM) (NÃO)')
+
+# CONDIÇÃO / PROCESSAMENTO
+if decisao == 'sim' or 'SIM':
+    print('Número inteiro aleatório: ')
+    numero_aleatorio2 = random.randint(1, 100)
+    print(f'Número gerado: {numero_aleatorio}')
+    
+    escolha = input('Escolhha uma opção:'
+          'a)SOMA'
+          'b)DIVISÃO'
+          'c)MULTIPLICAÇÃO'
+          'd)SUBTRAÇÃO')
+    if escolha == 'a':
+        print('SOMA')
+        soma = numero_aleatorio + numero_aleatorio2
+    else:
+        print(f'{numero_aleatorio} é IMPAR.') 
+else:
+    print('Fim do Programa')
+    print('-'*70)
